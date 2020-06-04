32f8ca3d7f2bc356fb11a6aa15e1b1e6a2797f1f
vantihovich <vantihovich@lyft.com>
2020-06-04 17:14:13 +0300

    changed the name of tested function to capitalazed 1st letter

--- web_improve/web_test.go
+++ web_improve/web_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestsqrtStr(t *testing.T) {
+func TestSqrtStr(t *testing.T) {
 	var want = float64(2)
 	if got, _ := sqrtStr("4"); got != want {
 		t.Errorf("sqrtStr() = %v want %v", got, want)
